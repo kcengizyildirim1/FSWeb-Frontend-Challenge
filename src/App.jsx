e6add891cff1149f2ac191f9cd1a968523c0b114
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-11-06 20:54:53 +0300

    Header kisminda dark mode button ve navbar eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return (
-    <div className="border-b-2 border-black">
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-    </div>
-  );
+  return <Header />;
 };
 
 export default App;
