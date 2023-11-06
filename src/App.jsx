c8a30220184e174642c3f9f54fe1cd6bbfd7508e
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-11-06 10:34:28 +0300

    Created Project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="border-b-2 border-black">
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
